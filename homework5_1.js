8bc1ca3462b57eac160f2ef140e473279fcd5cf9
Kyrylo Romanov <romanovkirill@ukr.net>
2025-10-08 21:22:15 +0300

    bag fixed

--- homework5_1.js
+++ homework5_1.js
@@ -1,5 +1,5 @@
 let numbers = "";
 for (let i = 20; i <= 30; i += 0.5) {
-  numbers += i + " ";
+  numbers += i < 30 ? i + " " : i;
 }
 console.log(numbers);
---
+++ homework5_4.js
@@ -0,0 +1,1 @@
+const num = prompt("Enter a number");
