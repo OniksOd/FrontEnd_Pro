dce620426a14be5c277a24c886efc80b7ca5f967
Kyrylo Romanov <romanovkirill@ukr.net>
2025-10-08 22:54:41 +0300

    made hw5_4

--- homework5_4.js
+++ homework5_4.js
@@ -1,1 +1,10 @@
 const num = prompt("Enter a number");
+for (i = 2; i < num; i++) {
+  if (num % i === 0) {
+    console.log("Not a prime number");
+    break;
+  }
+  if (i === num - 1) {
+    console.log("Is a prime number");
+  }
+}
