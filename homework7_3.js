e9ed6a688b7428d8298b6bc3b9315e48f30168fd
Kyrylo Romanov <romanovkirill@ukr.net>
2025-10-11 23:55:23 +0300

    tried different ways

--- homework7_3.js
+++ homework7_3.js
@@ -4,8 +4,18 @@
 // console.log(newArr);
 const arr = [1, 3, 4, 6, 2, 5, 7];
 const item = 4;
-function deleteItem(arr, item) {
-  arr.splice(arr.indexOf(item), 1);
+// function deleteItem(arr, item) {
+//   arr.splice(arr.indexOf(item), 1);
+//   return arr;
+// }
+// console.log(deleteItem(arr, item));
+function removeItem(arr, item) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === item) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
   return arr;
 }
-console.log(deleteItem(arr, item));
+console.log(removeItem(arr, item));
