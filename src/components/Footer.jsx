13f71b70cef8b839ff6e8f7dfeb06c791d9eaa71
Kyrylo Romanov <romanovkirill@ukr.net>
2026-01-15 19:58:04 +0200

    bag fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,22 @@
-import { Link, Box } from "@mui/material";
+import { Link, Box, Typography, List, ListItem } from "@mui/material";
 
 export const Footer = () => (
   <Box component="footer" className="footer">
-    <h1>My Blog</h1>
-    <ul>
-      <li>
+    <Typography variant="h2">My Blog</Typography>
+    <List>
+      <ListItem>
         Phone Number: <Link href="tel: +380636031826">+380636031826</Link>
-      </li>
-      <li>
+      </ListItem>
+      <ListItem>
         Email:
         <Link href="mailto: romanovkirill@ukr.net">romanovkirill@ukr.net</Link>
-      </li>
-      <li>
+      </ListItem>
+      <ListItem>
         Git:
         <Link href="https://github.com/OniksOd">
           https://github.com/OniksOd
         </Link>
-      </li>
-    </ul>
+      </ListItem>
+    </List>
   </Box>
 );
